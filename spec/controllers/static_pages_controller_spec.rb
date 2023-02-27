d3c1e845a1b49bef0092741958a84137300b342d
“tim” <litidm@gmail.com>
2023-02-27 16:32:09 +0300

    Fix bug with redirect when authenticate and stub for controller specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #about' do
+    let(:user) { create(:user) }
+
+    before { login(user) }
+
     it 'returns http success' do
       get :about
       expect(response).to have_http_status(:success)
