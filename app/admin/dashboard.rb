82715fd2be9c75add243eb84b1c8207db276615f
“tim” <litidm@gmail.com>
2023-03-01 14:21:13 +0300

    Add posts comments and users to admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "User" do
+          li link_to('Users', admin_users_path)
+        end
+      end
+
+      column do
+        panel "Posts" do
+          li link_to('Posts', admin_posts_path)
+        end
+      end
+
+      column do
+        panel "Users comments" do
+          li link_to('Users comments', admin_post_comments_path)
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
